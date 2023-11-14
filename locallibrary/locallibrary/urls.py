a9f485c187b7ef418c3ae8881ff10b5cea45b74c
Studentikitok <108453686+straponbace@users.noreply.github.com>
2023-11-14 13:01:27 +0700

    Add files via upload

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -15,22 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-# Используйте include() чтобы добавлять URL из каталога приложения
-from django.urls import include
-from django.urls import path
-urlpatterns += [
-     path('catalog/', include('catalog.urls')),
-]
-
-# Используйте static() чтобы добавить соотношения для статических файлов
-# Только на период разработки
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', RedirectView.as_view(url='catalog/')),
+    path('catalog/', include('catalog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
